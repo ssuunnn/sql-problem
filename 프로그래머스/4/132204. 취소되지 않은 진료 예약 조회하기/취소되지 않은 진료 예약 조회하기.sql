00fb7fb0f16ee7250bba29eb18c5775bde2fcc90
Sunwoo Nam <108582620+ssuunnn@users.noreply.github.com>
2025-08-23 22:12:00 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,27 @@
+SELECT
+    A.APNT_NO,
+    P.PT_NAME,
+    A.PT_NO,
+    A.MCDP_CD,
+    D.DR_NAME,
+    A.APNT_YMD
+FROM
+    APPOINTMENT A
+JOIN
+    PATIENT P
+    ON A.PT_NO = P.PT_NO
+JOIN
+    DOCTOR D
+    ON A.MDDR_ID = D.DR_ID
+WHERE
+    YEAR(A.APNT_YMD) = 2022
+    AND
+    MONTH(A.APNT_YMD) = 4
+    AND
+    DAY(A.APNT_YMD) = 13
+    AND
+    A.APNT_CNCL_YN = 'N'
+    AND
+    A.MCDP_CD = 'CS'
+ORDER BY
+    6;
